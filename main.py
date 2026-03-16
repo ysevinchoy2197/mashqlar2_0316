b0c5262f93352c6472b7f0c6bbcb35a74fb6028c
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2026-03-16 16:21:45 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#1-misol
+a = list((1,2,3,4))
+print(a)
+
+#2-misol
+a = [10, 20, 30]
+print(a)
+
+#3-misol
+davlatlar = ["Uzbekistan", "Kazakhstan"]
+print(davlatlar)
+
+davlatlar.append("Azarbadjon")
+print(davlatlar)
+
+#4-misol
+a = [7, 8, 9]
+print(a)
+print(f"Uzunligni toping: {len(a)}")
+
+#5-misol
+a = [1, 2, 3, 4]
+print(a)
+for i in a:
+    print(i)
